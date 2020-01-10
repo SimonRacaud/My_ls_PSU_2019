d549d8f8a3b2217525755d3b215be47eadb892f0
RACAUD Simon <simonracaud@gmail.com>
2020-01-10 09:26:03 +0100

    FIX(standard error)

--- src/file_data/get_file_type.c
+++ src/file_data/get_file_type.c
@@ -7,17 +7,6 @@
 
 #include "my_ls.h"
 
-/*
-S_IFMT = mask type
-S_IFSOCK   0140000   socket
-S_IFLNK    0120000   symbolic link
-S_IFREG    0100000   regular file
-S_IFBLK    0060000   block device
-S_IFDIR    0040000   directory
-S_IFCHR    0020000   character device
-S_IFIFO    0010000   FIFO
-*/
-
 static const int NB_FILETYPE = 7;
 static const char FILETYPE_CHAR[] =
 {
